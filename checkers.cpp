ba5a85d3ace9f6bc92e7a28018d1ac696f74983b
Graeme Ferguson <ggmferguson@gmail.com>
2018-04-15 22:40:02 -0400

    add board sprite

--- checkers.cpp
+++ checkers.cpp
@@ -3,13 +3,22 @@
 
 int main() {
     // Initialize popup window, i.e. the game window
-    sf::RenderWindow window(sf::VideoMode(520,520), "Checkers");
+    sf::RenderWindow window(sf::VideoMode(840,840), "Checkers");
     // Initialize background shape and look
-    sf::RectangleShape background(sf::Vector2f(480,480));
+    sf::RectangleShape background(sf::Vector2f(800,800));
     background.setFillColor(sf::Color::Black);
     background.setOutlineThickness(20);
     background.setOutlineColor(sf::Color::Red);
     background.setPosition(20,20);
+    // Initialize board texture
+    sf::Texture b_texture;
+    if (!b_texture.loadFromFile("c_board800.png")) {
+        std::cout << "Error importing board texture!" << std::endl;
+    }
+    // Initialize board sprite
+    sf::Sprite board;
+    board.setTexture(b_texture);
+    board.setPosition(20,20);
     // Creates game window instance
     while (window.isOpen()) {
         // Tracks interactions with window
@@ -38,6 +47,7 @@ int main() {
         }
         window.clear(); // Clears window for drawing on clean slate
         window.draw(background); // Draws background
+        window.draw(board); // Draws board sprite
         window.display();
     }
     return 0;
