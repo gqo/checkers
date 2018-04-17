4b5a87e011fde8f098ebeab998febe55008016a8
Graeme Ferguson <ggmferguson@gmail.com>
2018-04-16 21:13:09 -0400

    add click registration and remove background

--- checkers.cpp
+++ checkers.cpp
@@ -2,15 +2,23 @@
 #include <iostream>
 #include <vector>
 
+// class piece 
+// {
+// public:
+//     const bool color;
+//     sf::CircleShape shape;
+    
+// };
+
 // Function for initializing game pieces (unnecessary, but ugly otherwise)
 void initPieces(std::vector<sf::CircleShape>& pieces) {
-    int y = 30; // y-axis starting place
-    int x = 130; // x-axis starting place
+    int y = 10; // y-axis starting place
+    int x = 110; // x-axis starting place
     // For loop for creation of white pieces
     for(int i = 0; i < 6; i++) {
         if(i == 3){
-            y = 130;
-            x = 30;
+            y = 110;
+            x = 10;
         }
         sf::CircleShape piece_wh(40);
         piece_wh.setFillColor(sf::Color::White);
@@ -20,12 +28,12 @@ void initPieces(std::vector<sf::CircleShape>& pieces) {
         pieces.push_back(piece_wh);
         x += 200;
     }
-    y = 430;
-    x = 130;
+    y = 410;
+    x = 110;
     for(int i = 0; i < 6; i++) {
         if(i == 3){
-            y = 530;
-            x = 30;
+            y = 510;
+            x = 10;
         }
         sf::CircleShape piece_bl(40);
         piece_bl.setFillColor(sf::Color::Black);
@@ -45,14 +53,9 @@ void drawPieces(std::vector<sf::CircleShape>& pieces, sf::RenderWindow& window) 
 }
 
 int main() {
-    // Initialize popup window, i.e. the game window
-    sf::RenderWindow window(sf::VideoMode(640,640), "Checkers"); // (x-axis, y-axis)
-    // Initialize background shape and look
-    sf::RectangleShape background(sf::Vector2f(600,600));
-    background.setFillColor(sf::Color::Black);
-    background.setOutlineThickness(20);
-    background.setOutlineColor(sf::Color::Red);
-    background.setPosition(20,20);
+    // Initialize popup window, i.e. the game window. Note: cannot resize window
+    sf::RenderWindow window(sf::VideoMode(600,600), "Checkers", 
+        sf::Style::Titlebar | sf::Style::Close);
     // Initialize board texture
     sf::Texture b_texture;
     if(!b_texture.loadFromFile("c_board600.png")) {
@@ -61,22 +64,19 @@ int main() {
     // Initialize board sprite
     sf::Sprite board;
     board.setTexture(b_texture);
-    board.setPosition(20,20);
-    // Initialize white checker piece
-    // sf::CircleShape piece_wh(40);
-    // piece_wh.setFillColor(sf::Color::White);
-    // piece_wh.setOutlineThickness(5);
-    // piece_wh.setOutlineColor(sf::Color(80,80,80)); // Grey outline
-    // // piece_wh.setPosition(130,30);
-    // Initialize black checker piece
-    // sf::CircleShape piece_bl(40);
-    // piece_bl.setFillColor(sf::Color::Black);
-    // piece_bl.setOutlineThickness(5);
-    // piece_bl.setOutlineColor(sf::Color(80,80,80)); // Grey outline
-    // piece_bl.setPosition(30,530);
-    // Initialize vector for tracking pieces
+    board.setPosition(0,0);
+    // Initialize vector for tracking pieces and initialize them
     std::vector<sf::CircleShape> pieces;
     initPieces(pieces);
+    // Initialize highlight square
+    sf::RectangleShape hlight(sf::Vector2f(94,94));
+    hlight.setFillColor(sf::Color::Transparent);
+    hlight.setOutlineThickness(3);
+    hlight.setOutlineColor(sf::Color::Red);
+    hlight.setPosition(103,103);
+    // Initialize ints for tracking co-ords of board clicks
+    int m_xpos = 0;
+    int m_ypos = 0;
     // Creates game window instance
     while(window.isOpen()) {
         // Tracks interactions with window
@@ -98,16 +98,45 @@ int main() {
                             break;
                     }
                     break;
+                case sf::Event::MouseButtonPressed:
+                    // Prints mouse position on left click, otherwise break
+                    if(event.mouseButton.button == sf::Mouse::Left) {
+                        std::cout << "Left click!" << std::endl;
+                        std::cout << "Mouse X: " << event.mouseButton.x << std::endl;
+                        std::cout << "Mouse Y: " << event.mouseButton.y << std::endl;
+                        // Gets x and y co-ords of board of mouse click
+                        m_xpos = event.mouseButton.x / 100;
+                        m_ypos = event.mouseButton.y / 100;
+                        // Sets highlight box to last square clicked
+                        hlight.setPosition(m_xpos*100+3,m_ypos*100+3);
+                    }
+                    break;
                 // Default for unhandled event type
                 default:
                     break;
             }
         }
         window.clear(); // Clears window for drawing on clean slate
-        window.draw(background); // Draws background
         window.draw(board); // Draws board sprite
+        window.draw(hlight);
         drawPieces(pieces,window); // Draws game pieces
         window.display();
     }
     return 0;
 }
+
+/* Notes
+
+[0][1][2][3][4][5 ]
+[6][7][8]......[11]
+[12]...........[17]
+[18]...........[23]
+[24]...........[29]
+[30]...........[35]
+
+(30,30) (30,130) (30,230)
+(130,30)(130,130)(130,230)
+
+(x-axis, y-axis) for any co-ord pair in SFML
+
+*/
