8c62d21cf95b883a7105aba6f750622fb97924eb
Graeme Ferguson <ggmferguson@gmail.com>
2018-04-18 13:40:55 -0400

    add prototype regular move prediction

--- checkers.cpp
+++ checkers.cpp
@@ -10,6 +10,66 @@
     
 // };
 
+// Function for checking piece type at board co-ords
+// Pass y co-ordinate data to row, x co-ordinate data to col
+int getPiece(int& row, int& col, int (&b_array)[6][6]) {
+    std::cout << "Got piece at (" << row << "," << col << "): " << b_array[row][col];
+    std::cout << std::endl;
+    return b_array[row][col];
+}
+
+// Checks if highlight is valid for future moves
+// Left = 0 for side
+bool checkValid(int& row, int& col, int& piece_type, bool side) {
+    if(!side && (col-1 < 0)) { return 0; }
+    else if(side && (col+1 > 5)) { return 0; }
+    switch(piece_type) {
+        case 0:
+            return 0;
+            break;
+        case 1:
+            if(row+1 > 5) { return 0; }
+            else { return 1; }
+            break;
+        case 2:
+            if(row-1 < 0) { return 0; }
+            else { return 1; }
+            break;
+        default:
+            break;
+    }
+}
+
+void drawPossibles(int& xcord, int& ycord, int& piece_type, sf::RenderWindow& window, 
+    sf::RectangleShape& phlight) {
+    switch(piece_type) {
+        case 0:
+            break;
+        case 1:
+            if(checkValid(ycord,xcord,piece_type,0)) {
+                phlight.setPosition((xcord-1)*100+3,(ycord+1)*100+3);
+                window.draw(phlight);
+            }
+            if(checkValid(ycord,xcord,piece_type,1)) {
+                phlight.setPosition((xcord+1)*100+3,(ycord+1)*100+3);
+                window.draw(phlight);
+            }
+            break;
+        case 2:
+            if(checkValid(ycord,xcord,piece_type,0)) {
+                phlight.setPosition((xcord-1)*100+3,(ycord-1)*100+3);
+                window.draw(phlight);
+            }
+            if(checkValid(ycord,xcord,piece_type,1)) {
+                phlight.setPosition((xcord+1)*100+3,(ycord-1)*100+3);
+                window.draw(phlight);
+            }
+            break;
+        default:
+            break;
+    }
+}
+
 // Function for initializing game pieces (unnecessary, but ugly otherwise)
 void initPieces(std::vector<sf::CircleShape>& pieces) {
     int y = 10; // y-axis starting place
@@ -80,16 +140,36 @@ int main() {
     // Initialize vector for tracking pieces and initialize them
     std::vector<sf::CircleShape> pieces;
     initPieces(pieces);
-    // Initialize highlight square
+    // Initialize click highlight square
     sf::RectangleShape hlight(sf::Vector2f(94,94));
     hlight.setFillColor(sf::Color::Transparent);
     hlight.setOutlineThickness(3);
     hlight.setOutlineColor(sf::Color::Red);
     hlight.setPosition(103,103);
-    // Initialize ints for tracking co-ords of board clicks
-    int m_xpos = 0;
-    int m_ypos = 0;
+    // Initialize possible move highlight square
+    sf::RectangleShape phlight(sf::Vector2f(94,94));
+    phlight.setFillColor(sf::Color::Transparent);
+    phlight.setOutlineThickness(3);
+    phlight.setOutlineColor(sf::Color::Yellow);
+    phlight.setPosition(103,103);
+    // Initialize vars for tracking clicks
+        // Co-ords for board, set on click, from top left
+        int m_xcord_old = 0; 
+        int m_ycord_old = 0;
+        // int m_xcord_new = 0; 
+        // int m_ycord_new = 0;
+        // Tracks if piece held 
+        bool held = 0; // false = not held
+        bool hvalid = 0; // tracks if hlight should be drawn
+        // Current type of piece (black, white, nothing)
+        int piece_type = 0;
     // Intialize 2d board array
+    // Note: Board co-ordinates are classified as (row,column) as such it
+    // can be confusing that the x-axis and y-axis are switched between
+    // SFML recognition stuff and these (row,column) setups. Example:
+    // SFML mouse co-ordinates are given as (x,y) position of mouse in window
+    // from the top left. Essentially, x is the column and y is the row.
+    // So SFML (x,y) == b_array(y,x)
     int b_array[6][6] = 
     {
         {0,1,0,1,0,1},
@@ -128,11 +208,23 @@ int main() {
                     // Gets mouse co-ords and sets highlight on left click, 
                     // otherwise break
                     if(event.mouseButton.button == sf::Mouse::Left) {
-                        // Gets x and y co-ords of board of mouse click
-                        m_xpos = event.mouseButton.x / 100;
-                        m_ypos = event.mouseButton.y / 100;
-                        // Sets highlight box to last square clicked
-                        hlight.setPosition(m_xpos*100+3,m_ypos*100+3);
+                        if(!held) {
+                            // Gets x and y co-ords of board of mouse click
+                            m_xcord_old = event.mouseButton.x / 100;
+                            m_ycord_old = event.mouseButton.y / 100;
+                            // std::cout << "Clicked @ (" << m_xcord_old << ',';
+                            // std::cout << m_ycord_old << ')' << std::endl;
+                            // Sets highlight box to last square clicked
+                            hlight.setPosition(m_xcord_old*100+3,m_ycord_old*100+3);
+                            piece_type = getPiece(m_ycord_old,m_xcord_old,b_array);
+                            if(piece_type != 0) { hvalid = 1; } 
+                            else { hvalid = 0; }
+                            held = !held;
+                        } else {
+                            // m_xcord_new = event.mouseButton.x / 100;
+                            // m_ycord_new = event.mouseButton.y / 100;
+                            held = !held;
+                        }
                     }
                     break;
                 // Default for unhandled event type
@@ -142,7 +234,10 @@ int main() {
         }
         window.clear(); // Clears window for drawing on clean slate
         window.draw(board); // Draws board sprite
-        window.draw(hlight);
+        if(held && hvalid) { 
+            window.draw(hlight);
+            drawPossibles(m_xcord_old,m_ycord_old,piece_type,window,phlight);
+        }
         drawPieces(pieces,window); // Draws game pieces
         window.display();
     }
